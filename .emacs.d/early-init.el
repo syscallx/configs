a764185032d77f1576bda33e795b2ae490ce6156
Karim Urazimbetov <karim@debian>
2025-05-25 13:45:31 +0500

    Update emacs config

---
+++ .emacs.d/early-init.el
@@ -0,0 +1,12 @@
+(add-to-list 'default-frame-alist '(background-color . "#212121"))
+(add-to-list 'default-frame-alist '(foreground-color . "#eee"))
+(add-to-list 'default-frame-alist '(font . "Monospace-12:bold"))
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+
+
+(setq inhibit-startup-message t)
+(setq initial-scratch-message nil)
+(setq package-enable-at-startup nil)
+(setq frame-inhibit-implied-resize t)
